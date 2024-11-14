b90304110811bd80c08a65a7f4326178a9bab1b1
archcode-web <archcodeapp@gmail.com>
2024-11-14 10:20:21 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# blog_template_start
+# blog_template_start
+# blog_template_start
+# template-start
+# start-template
+# start-template
+# start-template
+# start-template
---
+++ footer.php
@@ -0,0 +1,12 @@
+  <!-- Footer-->
+  <footer class="py-5 bg-dark">
+      <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
+      </div>
+  </footer>
+
+  <!-- Bootstrap core JS-->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+
+  </html>
